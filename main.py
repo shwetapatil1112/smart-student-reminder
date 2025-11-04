00adc584462529dbfff2a8d5c71a7a6012182f15
shweta Dhananjay Patil <shwetapatil7946@gmail.com>
2025-11-04 20:04:45 +0530

    Add files via upload
    Day 3: Stored reminder in SQLite database

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from tkinter import *
+import sqlite3
+
+conn = sqlite3.connect("reminders.db")
+c = conn.cursor()
+c.execute("""CREATE TABLE IF NOT EXISTS reminders (
+    title TEXT,
+    date TEXT,
+    time TEXT
+)""")
+conn.commit()
 
 root = Tk()
 root.title("Smart Student Reminder System")
 root.geometry("400x400")
 
-# App heading
 Label(root, text="Smart Student Reminder System", font=("Arial", 16, "bold")).pack(pady=20)
 
-# -----------------------------
-# Day 2: Add input fields
-# -----------------------------
 Label(root, text="Reminder Title").pack()
 title_entry = Entry(root, width=30)
 title_entry.pack()
@@ -22,7 +28,16 @@ Label(root, text="Time (HH:MM)").pack()
 time_entry = Entry(root, width=30)
 time_entry.pack()
 
-Button(root, text="Add Reminder").pack(pady=10)
+def add_reminder():
+    title = title_entry.get()
+    date = date_entry.get()
+    time = time_entry.get()
 
-# -----------------------------
+    c.execute("INSERT INTO reminders VALUES (?, ?, ?)", (title, date, time))
+    conn.commit()
+
+    Label(root, text="✅ Reminder Saved!", fg="green").pack()
+
+Button(root, text="Add Reminder", command=add_reminder).pack(pady=10)
+
 root.mainloop()
